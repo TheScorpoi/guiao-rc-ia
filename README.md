d31c2f50ece5e59654c9901090571153607896ad
TheScorpoi <sobral@ua.pt>
2021-11-29 16:01:09 +0000

    ex aula6 and aula7

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # iia-ia-guiao-rc
+
+pip install flake8 -> flake8 file.py -> para ver os erros de tipos e cenas
+pip install black -> para identar codigo
--- semantic_network.py
+++ semantic_network.py
@@ -2,7 +2,7 @@
 
 # Guiao de representacao do conhecimento
 # -- Redes semanticas
-# 
+#
 # Inteligencia Artificial & Introducao a Inteligencia Artificial
 # DETI / UA
 #
@@ -18,30 +18,34 @@
 #
 
 class Relation:
-    def __init__(self,e1,rel,e2):
+    def __init__(self, e1, rel, e2):
         self.entity1 = e1
 #       self.relation = rel  # obsoleto
         self.name = rel
         self.entity2 = e2
+
     def __str__(self):
         return self.name + "(" + str(self.entity1) + "," + \
-               str(self.entity2) + ")"
+            str(self.entity2) + ")"
+
     def __repr__(self):
         return str(self)
 
 
 # Subclasse Association
 class Association(Relation):
-    def __init__(self,e1,assoc,e2):
-        Relation.__init__(self,e1,assoc,e2)
+    def __init__(self, e1, assoc, e2):
+        Relation.__init__(self, e1, assoc, e2)
 
 #   Exemplo:
 #   a = Association('socrates','professor','filosofia')
 
 # Subclasse Subtype
+
+
 class Subtype(Relation):
-    def __init__(self,sub,super):
-        Relation.__init__(self,sub,"subtype",super)
+    def __init__(self, sub, super):
+        Relation.__init__(self, sub, "subtype", super)
 
 
 #   Exemplo:
@@ -49,8 +53,8 @@ class Subtype(Relation):
 
 # Subclasse Member
 class Member(Relation):
-    def __init__(self,obj,type):
-        Relation.__init__(self,obj,"member",type)
+    def __init__(self, obj, type):
+        Relation.__init__(self, obj, "member", type)
 
 #   Exemplo:
 #   m = Member('socrates','homem')
@@ -59,12 +63,16 @@ class Member(Relation):
 # -- associa um utilizador a uma relacao por si inserida
 #    na rede semantica
 #
+
+
 class Declaration:
-    def __init__(self,user,rel):
+    def __init__(self, user, rel):
         self.user = user
         self.relation = rel
+
     def __str__(self):
         return "decl("+str(self.user)+","+str(self.relation)+")"
+
     def __repr__(self):
         return str(self)
 
@@ -77,23 +85,129 @@ class Declaration:
 # -- composta por um conjunto de declaracoes
 #    armazenado na forma de uma lista
 #
+
+
 class SemanticNetwork:
-    def __init__(self,ldecl=None):
-        self.declarations = [] if ldecl==None else ldecl
+    def __init__(self, ldecl=None):
+        self.declarations = [] if ldecl == None else ldecl
+
     def __str__(self):
         return str(self.declarations)
-    def insert(self,decl):
+
+    def insert(self, decl):
         self.declarations.append(decl)
-    def query_local(self,user=None,e1=None,rel=None,e2=None):
+
+    def query_local(self, user=None, e1=None, rel=None, e2=None, type=None):
         self.query_result = \
-            [ d for d in self.declarations
-                if  (user == None or d.user==user)
+            [d for d in self.declarations
+                if (user == None or d.user == user)
                 and (e1 == None or d.relation.entity1 == e1)
                 and (rel == None or d.relation.name == rel)
-                and (e2 == None or d.relation.entity2 == e2) ]
+                and (e2 == None or d.relation.entity2 == e2)
+                and (type == None or isinstance(d.relation, type))]
+            
         return self.query_result
+
     def show_query_result(self):
         for d in self.query_result:
             print(str(d))
 
+    def list_associations(self):
+        return list(set(i.relation.name for i in self.declarations if isinstance(i.relation, Association)))
 
+    def list_objects(self):
+        return list(set(i.relation.entity1 for i in self.declarations if isinstance(i.relation, Member)))
+
+    def list_users(self):
+        return list(set(i.user for i in self.declarations))
+
+    def list_types(self):
+        lst = []
+        for i in self.declarations:
+            if isinstance(i.relation, (Subtype, Member)):
+                lst.append(i.relation.entity2)
+            if isinstance(i.relation, Subtype):
+                lst.append(i.relation.entity2)
+        return set(lst)
+
+    def list_local_associations(self, entity):
+        lst = []
+        for i in self.declarations:
+            if isinstance(i.relation, Association):
+                if entity in [i.relation.entity1, i.relation.entity2]:
+                    lst.append(i.relation.name)
+        return set(lst)
+
+    def list_relations_by_user(self, user):
+        lst = []
+        for i in self.declarations:
+            if user == i.user:
+                lst.append(i.relation.name)
+        return set(lst)
+    
+    def associations_by_user(self, user):
+        lst = []
+        for i in self.declarations:
+            if isinstance(i.relation,Association):
+                if user == i.user:
+                    lst.append(i.relation.name)
+        return len(set(lst))
+    
+    def list_local_associations_by_user(self, entity):
+        lst = []
+        for i in self.declarations:
+            if isinstance(i.relation, Association):
+                if entity in [i.relation.entity1, i.relation.entity2]:
+                    lst.append((i.relation.name, i.user))
+        return set(lst)
+    
+    def predecessor(self, ent1, ent2):
+        for i in self.declarations:
+            if isinstance(i.relation, (Member, Subtype)) and ent2 == i.relation.entity1:
+                if i.relation.entity2 == ent1 or self.predecessor(ent1, i.relation.entity2):
+                    return True
+        return False
+    
+    def predecessor_path(self, ent1, ent2):
+        for i in self.declarations:
+            if isinstance(i.relation, (Member, Subtype)) and ent2 == i.relation.entity1:
+                if i.relation.entity2 == ent1:
+                    return [ent1, ent2]
+                return self.predecessor_path(ent1, i.relation.entity2) + [ent2]
+            
+    def query(self, entity, assoc=None):
+        pds_assoc = []
+        pds = self.query_local(e1=entity, type=(Member, Subtype))
+        for ent2 in [d.relation.entity2 for d in pds]:
+            pds_assoc.extend(self.query(ent2, assoc))
+        local_assoc = self.query_local(e1=entity, rel=assoc, type=Association)
+        return pds_assoc + local_assoc
+    
+    def query2(self, entity, rel=None):
+        pds_assoc = []
+        pds = self.query_local(e1=entity, type=(Member, Subtype))
+        for ent2 in [d.relation.entity2 for d in pds]:
+            pds_assoc.extend(self.query(ent2, rel))
+        local_assoc = self.query_local(e1=entity, rel=rel)
+        return pds_assoc + local_assoc
+    
+    def query_cancel(self, entity, assoc=None):
+        pds_assoc = []
+        local_assoc = self.query_local(e1=entity, rel=assoc)
+        pds = self.query_local(e1=entity, type=(Member, Subtype))
+        for ent2 in [d.relation.entity2 for d in pds]:
+            pds_assoc.extend([d for d in self.query_cancel(ent2, assoc) if d.relation.name not in [l.relation.name for l in local_assoc]])
+        return pds_assoc + local_assoc
+        
+    def query_down(self, entity, assoc, first=True):
+        decendents_assoc = []
+        decendents = self.query_local(e2=entity, type=(Member, Subtype))
+        for e1  in [d.relation.entity1 for d in decendents]:
+            decendents_assoc.extend(self.query_down(e1, assoc, False))
+        if first:
+            local_assoc = []
+        else:        
+            local_assoc = self.query_local(e1=entity, rel=assoc)
+        return decendents_assoc + local_assoc
+    
+    
