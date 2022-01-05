5a270c271e2206b1ceb6bb6d22abd7e6621394be
TheScorpoi <sobral@ua.pt>
2022-01-05 21:22:28 +0000

    update some exercices

--- bayes_net.py
+++ bayes_net.py
@@ -1,5 +1,4 @@
 
-
 class BayesNet:
 
     def __init__(self, ldep=None):  # Why not ldep={}? See footnote 1.
@@ -27,6 +26,22 @@ class BayesNet:
                 if mothers.issubset(conjunction):
                     prob*=(p if val else 1-p)
         return prob
+
+    def individualProb(self, var, value):
+        variables = [v for v in self.dependencies.keys() if v != var ]
+
+        return sum([self.jointProb([(var, value)] + c) for c in self._gen_conj(variables)])
+    
+    def _gen_conj(self, variables):
+        if len(variables)== 1:
+            return [[(variables[0],True)],[(variables[0],False)]]
+
+        conj = []
+        remaining = self._gen_conj(variables[1:])
+        for r in remaining:
+            conj.append([(variables[0], True)] + r)
+            conj.append([(variables[0], False)] + r)
+        return conj
 
 
 # Footnote 1:
